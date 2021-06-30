0d7bec3bf376d4c09215e4f77d9088ac589031af
X Y Z <rozbujnik@gmail.com>
2021-06-30 08:48:24 +0200

    Update DrawComplex.h

--- src/Images/DrawComplex.h
+++ src/Images/DrawComplex.h
@@ -1,3 +1,11 @@
+/*!
+This class allows to connect state with a draw item.
+
+For example
+STATE_STANDING : gif1.gif
+STATE_MOVING_RIGHT : gif2.gif
+...
+*/
 
 class DrawComplex : public DrawItem
 {
